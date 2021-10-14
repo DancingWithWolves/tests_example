aacc64bce8874584d379890773f9678256ba508f
dancing_with_wolves <dimikson4@gmail.com>
2021-10-14 21:43:18 +0300

    typo fixed

--- tests_example.cpp
+++ tests_example.cpp
@@ -6,13 +6,13 @@ void run_all_tests() { assert(OK == test_echo()); }
 Error test_echo() {
   int test_value = 10;
   if (test_value != echo(test_value)) {
-    printf("F*ck this th*t I'm out\n");
+    printf("F*ck this sh*t I'm out\n");
     return ligma;
   }
 
   test_value = 0;
   if (test_value != echo(test_value)) {
-    printf("F*ck this th*t at this point I'm out\n");
+    printf("F*ck this sh*t at this point I'm out for sure\n");
     return ligma;
   }
 
