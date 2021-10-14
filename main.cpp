c60079d7eaed7835e618015ff173b14e2232d16e
dancing_with_wolves <dimikson4@gmail.com>
2021-10-14 21:35:58 +0300

    more talkative ones

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@
 
 int main() {
   run_all_tests();
+  printf("All tests passed!\n");
   return 0;
 }
--- tests_example.cpp
+++ tests_example.cpp
@@ -1,15 +1,18 @@
 #include "tests_example.h"
 #include <assert.h>
+#include <stdio.h>
 
 void run_all_tests() { assert(OK == test_echo()); }
 Error test_echo() {
   int test_value = 10;
   if (test_value != echo(test_value)) {
+    printf("F*ck this th*t I'm out\n");
     return ligma;
   }
 
   test_value = 0;
   if (test_value != echo(test_value)) {
+    printf("F*ck this th*t at this point I'm out\n");
     return ligma;
   }
 
